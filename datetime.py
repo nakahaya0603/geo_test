473d45612a9f66a28cd93a8928431c64b046a7c7
nakahaya0603 <77122499+nakahaya0603@users.noreply.github.com>
2023-12-17 22:21:00 +0900

    Add files via upload

---
+++ datetime.py
@@ -0,0 +1,13 @@
+import datetime
+import json
+
+t_delta = datetime.timedelta(hours=9)
+JST = datetime.timezone(t_delta, 'JST')
+now = datetime.datetime.now(JST)
+d = now.strftime('%Y/%m/%d %H:%M:%S')
+print(d)
+
+data = {'last_update': d}
+
+with open('last_update.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False)
